8d488c32c79426706aef4ada2ec8e558e89a95d8
Fernando Stecconi <fernandostecconi@Fernandos-MacBook-Pro.local>
2020-12-28 21:04:18 +0100

    wip [ci skip]

--- bin/shell-start.zsh
+++ bin/shell-start.zsh
@@ -1,31 +1,36 @@
 # Update!
-pushd ~/shell-config; git pull; popd
+#pushd ~/shell-config; git pull; popd
 
 # Python
-PATH=~/Library/Python/2.7/bin:$PATH
+#PATH=~/Library/Python/2.7/bin:$PATH
 
 # Pipx (more generic?)
-PATH=~/.local/bin:$PATH
+#PATH=~/.local/bin:$PATH
 
 # Pyenv
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+#export PATH="$HOME/.pyenv/bin:$PATH"
+#eval "$(pyenv init -)"
+#eval "$(pyenv virtualenv-init -)"
 
 # PyInvoke
 # Generated using `inv --print-completion-script zsh`
-_complete_invoke() {
-    collection_arg=''
-    if [[ "${words}" =~ "(-c|--collection) [^ ]+" ]]; then
-        collection_arg=$MATCH
-    fi
-    reply=( $(invoke ${=collection_arg} --complete -- ${words}) )
-}
-compctl -K _complete_invoke + -f invoke inv
-# vim: set ft=sh :
+#_complete_invoke() {
+#    collection_arg=''
+#    if [[ "${words}" =~ "(-c|--collection) [^ ]+" ]]; then
+#        collection_arg=$MATCH
+#    fi
+#    reply=( $(invoke ${=collection_arg} --complete -- ${words}) )
+#}
+#compctl -K _complete_invoke + -f invoke inv
+## vim: set ft=sh :
 
 # Rbenv
-#eval "$(rbenv init -)"
+eval "$(rbenv init -)"
+
+# FZF
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export FZF_DEFAULT_COMMAND="find . -type f -not -path '*/\.git/*'"
+
 
 # Me
 PATH=~/bin:$PATH
