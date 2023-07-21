3994f36e4f1a81b7bed7245d187722183cee1afa
Fernando Stecconi <nandilugio@gmail.com>
2023-07-21 20:04:00 +0200

    merge

--- bin/shell-start.zsh
+++ bin/shell-start.zsh
@@ -5,6 +5,10 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND="find . -type f -not -path '*/\.git/*'"
 
+# Perl (and therefore pgtap, etc.)
+PATH=$PATH:"/usr/local/Cellar/perl/5.32.0/bin"
+
+# Me
 PATH=~/bin:$PATH
 PS1=$PS1$'\n'"%# "  # Newline after prompt
 setopt HIST_IGNORE_SPACE
