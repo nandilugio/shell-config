1ea2e31ea8cef66d85b95eff213131c4b02707ab
Fernando Stecconi <nandilugio@gmail.com>
2025-05-23 12:53:38 +0200

    wip [ci skip]

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -22,6 +22,12 @@
 
 -- ==============================================================================
 
+-- Use 4 spaces instead of tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
 -- [[ Kickstart config ]]
 -- https://github.com/nvim-lua/kickstart.nvim/tree/master
 
