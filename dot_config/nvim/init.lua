dd573f2b42b60563cc238991c49f53f4620c95d9
Fernando Stecconi <nandilugio@gmail.com>
2025-06-25 00:52:40 +0200

    wip [ci skip]

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -61,7 +61,7 @@ vim.o.mouse = "a"
 -- Files --
 
 -- Save undo history
-vim.o.undofile = true
+-- vim.o.undofile = true
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
